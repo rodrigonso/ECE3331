8f5c84bd4591abae8e50d62303be172f9bae67b8
Rodrigo <rodrigocm08@gmail.com>
2020-09-12 12:47:44 -0500

    ANSI C version of problem

--- mascarenhas_rodrigo_3.cpp
+++ mascarenhas_rodrigo_3.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    bool scan = true;
+    FILE *outFile;
+    outFile = fopen("out.txt", "w");
+
+    while (scan)
+    {
+        float balance;
+        char name;
+
+        printf("Enter your name: \n");
+        int input = scanf("%c", &name);
+
+        fprintf(outFile, "%c", name);
+
+        if (name == '\n')
+        {
+            printf("Enter your balance:");
+            input = scanf("%f", &balance);
+            fprintf(outFile, "$%.2f\n======================", balance);
+            break;
+        }
+    }
+    fclose(outFile);
 }
