fb1b356b975f645acff9f0c95a4098a16cef7ff9
Rodrigo Mascarenhas <Rodrigo@erics-imac.attlocal.net>
2020-10-06 11:17:13 -0500

    bug fix

--- program_6/mascarenhas_rodrigo_6.cpp
+++ program_6/mascarenhas_rodrigo_6.cpp
@@ -3,26 +3,27 @@
 #include <time.h>
 #include <ctype.h>
 
-void initiate_probs(float *array);
+void calculate_probs(float *array);
 int main(int argc, char const *argv[])
 {
   float prob_interval[] = {0.2, 0.27, 0.22, 0.14, 0.09, 0.08};
-  initiate_probs(prob_interval);
+  calculate_probs(prob_interval);
 
   int num_letters = 2;
   srand((long)&num_letters);
   int num_words = rand() % (20 + 1 - 10) + 10;
 
-  for (int i = 0; i < num_words; i++)
+  int i, j, k;
+  for (i = 0; i < num_words; i++)
   {
     float x = rand() / (float)(RAND_MAX);
 
-    for (int j = 1; j < 6; j++)
+    for (j = 1; j < 6; j++)
     {
       if (x >= prob_interval[j - 1] && x <= prob_interval[j])
         num_letters = j + 2;
     }
-    for (int k = 0; k < num_letters; k++)
+    for (k = 0; k < num_letters; k++)
     {
       int letter = rand() % (122 + 1 - 97) + 97;
       printf("%c", i == 0 && k == 0 ? toupper(letter) : letter);
@@ -32,9 +33,10 @@ int main(int argc, char const *argv[])
   return 0;
 }
 
-void initiate_probs(float *array)
+void calculate_probs(float *array)
 {
-  for (int i = i; i < 6; i++)
+  int i;
+  for (i = 1; i < 6; i++)
   {
     array[i] += array[i - 1];
   }
